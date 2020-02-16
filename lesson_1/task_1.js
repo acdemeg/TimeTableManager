2f821cbce89e3cd7574bc7d752befe6ae899b0ae
valerii_krylov <valera-krylov96@mail.ru>
2020-02-16 11:51:52 +0600

    task_1

---
+++ lesson_1/task_1.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let consolePrint = () => {
+  let n = +process.argv[2];
+  for(let i = 1; i <= n; i++) {
+    if(i % 3 === 0) console.log('fizz');
+    else if(i % 5 === 0) console.log('buzz');
+    else if(i % 15 === 0) console.log('fizzbuzz');
+    else console.log(i);
+  }
+}
+
+consolePrint();
